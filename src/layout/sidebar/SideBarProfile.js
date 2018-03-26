8905aed9ecbf52731b05f144765fd297a9df622e
Park, KyoungWook <kypark@ea.com>
2018-03-26 13:52:17 +0900

    Rename Profile in SideBar and Created Sidebar components

---
+++ src/layout/sidebar/SideBarProfile.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SideBarProfile = (props) => {
+    return (
+        <div className="profile clearfix">
+          <div className="profile_pic">
+            <img src="images/img.jpg" alt="..." className="img-circle profile_img"/>
+          </div>
+          <div className="profile_info">
+            <span>Welcome,</span>
+            <h2>John Doe</h2>
+          </div>
+        </div>
+    );
+}
+
+export default SideBarProfile;
