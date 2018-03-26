2a603514cbe6e7706d5c9ce984030cbd95e77086
박경욱 <kwpark@kwpark-MacBook-pro.local>
2018-03-27 05:50:56 +0900

    Changed TitleBar to function component

--- src/layout/sidebar/TitleBar.js
+++ src/layout/sidebar/TitleBar.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-class TitleBar extends React.Component {
-    render() {
-        return (
-            <div className="navbar nav_title noBorder">
-              <a href="index.html" className="site_title"><i className="fa fa-paw"></i> <span>Gentelella Alela!</span></a>
-            </div>
-        );
-    }
+const TitleBar = (props) => {
+    return (
+        <div className="navbar nav_title noBorder">
+            <a href="index.html" className="site_title"><i className="fa fa-paw"></i> <span>Gentelella Alela!</span></a>
+        </div>
+    );
 }
 
 export default TitleBar;
